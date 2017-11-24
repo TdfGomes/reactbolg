bb7f4427d6356eaafb77cbe727aa9ecf0552223f
Tiago Gomes <t.dfgomes@gmail.com>
2017-11-24 18:16:33 +0000

    change data format

--- src/reducers/commentsReducer.js
+++ src/reducers/commentsReducer.js
@@ -1,9 +1,9 @@
 import { REQUEST_ALL_COMMENTS } from "../actions/actionTypes";
 
-const comments = (state = [], action) => {
+const comments = (state = {}, action) => {
   switch (action.type) {
     case REQUEST_ALL_COMMENTS:
-      return state.concat(action.comments)
+      return Object.assign(state,action.comments)
     default:
       return state
   }
